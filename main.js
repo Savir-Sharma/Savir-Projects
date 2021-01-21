19f82e9d5e47be159bfb3ddfc9d531a3ed03fdb8
Savir-Sharma <savirsharma17@gmail.com>
2021-01-21 19:53:00 +0530

    HI
    HI

---
+++ main.js
@@ -0,0 +1,109 @@
+prediction_1="";
+prediction_2="";
+
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:'png',
+    png_quality:90
+});
+
+camera=document.getElementById("camera");
+Webcam.attach("#camera");
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML="<img id='captured_image' src='" + data_uri + "'/>";
+    });
+}
+
+console.log("ml5 version:",ml5.version);
+
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/garjPAC3-/model.json',modelLoaded);
+
+function modelLoaded()
+{
+    console.log("model loaded");
+}
+
+function speak()
+{
+    var synth = window.speechSynthesis;
+    speak_data_1="The first prediction is" + prediction_1;
+    speak_data_2="And The second prediction is" + prediction_2;
+    var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
+    synth.speak(utterThis);
+}
+
+function check()
+{
+    img=document.getElementById("captured_image");
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results)
+{
+    if(error)
+    {
+        console.error(error);
+    }
+    else
+    {
+        console.log(results);
+        document.getElementById("result_emotion_name").innerHTML=results[0].label;
+        document.getElementById("result_emotion_name2").innerHTML=results[1].label;
+        prediction_1=results[0].label;
+        prediction_2=results[1].label;
+        speak();
+
+        if(results [0].label == "&#x270C;")
+        {
+            document.getElementById("update_emoji").innerHTML="&#x270C;";
+        }
+
+        if(results [0].label == "&#x270B;")
+        {
+            document.getElementById("update_emoji").innerHTML="&#x270B;";
+        }
+
+        if(results [0].label == "&#x1F44D;")
+        {
+            document.getElementById("update_emoji").innerHTML="&#x1F44D;";
+        }
+
+        if(results [0].label == "&#x1F44E;")
+        {
+            document.getElementById("update_emoji").innerHTML="&#x1F44E;";
+        }
+
+        if(results [0].label == "&#x1F44C;")
+        {
+            document.getElementById("update_emoji").innerHTML="&#x1F44C;";
+        }
+        if(results [1].label == "&#x270C;")
+        {
+            document.getElementById("update_emoji2").innerHTML="&#x270C;";
+        }
+
+        if(results [1].label == "&#x270B;")
+        {
+            document.getElementById("update_emoji2").innerHTML="&#x270B;";
+        }
+
+        if(results [1].label == "&#x1F44D;")
+        {
+            document.getElementById("update_emoji2").innerHTML="&#x1F44D;";
+        }
+
+        if(results [1].label == "&#x1F44E;")
+        {
+            document.getElementById("update_emoji2").innerHTML="&#x1F44E;";
+        }
+
+        if(results [1].label == "&#x1F44C;")
+        {
+            document.getElementById("update_emoji2").innerHTML="&#x1F44C;";
+        }        
+    }
+}
